8bb5f5268da736eb3ce83b7dbf3859f4d0c26810
Root\vijayarasan.sivanandham <vijayarasan.sivanandham@syncfusion.com>
2020-03-18 16:54:58 +0530

    How to suspend the PING sound when perform the Tab operation in DataGrid(SfDataGrid)?

---
+++ BuzzerSound/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿using Syncfusion.WinForms.DataGrid;
+using Syncfusion.WinForms.DataGrid.Enums;
+using System;
+using System.Windows.Forms;
+
+namespace GettingStarted
+{
+    /// <summary>
+    /// Summary description for Form1.
+    /// </summary>
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.Container components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sfDataGrid = new Syncfusion.WinForms.DataGrid.SfDataGrid();
+            ((System.ComponentModel.ISupportInitialize)(this.sfDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sfDataGrid
+            // 
+            this.sfDataGrid.AccessibleName = "Table";
+            this.sfDataGrid.AllowDraggingColumns = true;
+            this.sfDataGrid.AllowFiltering = true;
+            this.sfDataGrid.AllowResizingColumns = true;
+            this.sfDataGrid.AllowResizingHiddenColumns = true;
+            this.sfDataGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.sfDataGrid.BackColor = System.Drawing.SystemColors.Window;
+            this.sfDataGrid.Location = new System.Drawing.Point(12, 1);
+            this.sfDataGrid.Name = "sfDataGrid";
+            this.sfDataGrid.SelectionMode = Syncfusion.WinForms.DataGrid.Enums.GridSelectionMode.Extended;
+            this.sfDataGrid.Size = new System.Drawing.Size(723, 332);
+            this.sfDataGrid.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(747, 361);
+            this.Controls.Add(this.sfDataGrid);
+            this.MinimumSize = new System.Drawing.Size(500, 400);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Getting Started";
+            ((System.ComponentModel.ISupportInitialize)(this.sfDataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+        #endregion
+
+        #region API Definition
+
+        private SfDataGrid sfDataGrid;
+
+        #endregion
+    }
+}
+
